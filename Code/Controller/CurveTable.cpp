8dc405acea1a27c2480dd9e67239e8edebf8dc6b
NSFW <nsfw@outlook.com>
2018-09-11 00:33:51 -0700

    Trying to get a lumpy idle.

--- Code/Controller/CurveTable.cpp
+++ Code/Controller/CurveTable.cpp
@@ -9,14 +9,15 @@ CurveTable *pFilterWeightTable = CurveTable::CreateRpmFilterTable();
 
 CurveTable * CurveTable::CreateExhaustCamTable()
 {
-	// Intake advance, for comparison               30.0     30.0     15.0      10.0
-	static float input[] = { 500.0f,  2000.0f, 3200.0f, 5600.0f,  8000.0f };
-	static float output[] = {  1.0f,     1.0f,    1.0f,   15.0f,    20.0f };
+	// Intake advance, for comparison   0.0     15.0      30.0    30.0     15.0      10.0
+	static float input[] = { 800.0f, 1000.0f, 1250.0f, 1500.0f, 3200.0f, 5600.0f,  8000.0f };
+	static float output[] = {  1.0f,    5.0f,    1.0f,    1.0f,    1.0f,   15.0f,    20.0f };
+
 	// With 15 degrees in cruise, (and 30 degrees intake advance), the engine ran rough.
-	// That would be 11 degrees of overlap @ 0.050. So no wonder it was rough!
+	// With BC 272s, that would be 11 degrees of overlap @ 0.050. So, no wonder.
 
 	return new CurveTable(
-		5,
+		7,
 		input,
 		output);
 }
@@ -24,8 +25,11 @@ CurveTable * CurveTable::CreateExhaustCamTable()
 CurveTable * CurveTable::CreateRpmFilterTable()
 {
 	// RPM needs to be filtered when low because it jumps around a lot at idle.
+	// The 0.7 value isn't final, it's just the second thing I tried and it worked well.
+	// At 0.1 (the first thing I tried) I could hear the idle getting lumpy from cam 
+	// retard before seeing the retard show up in the output, so that's way too low.
 	static float input[] = {  1100.0f,  2000.0f };
-	static float output[] = {    0.1f,     1.0f };
+	static float output[] = {    0.7f,     1.0f };
 
 	return new CurveTable(
 		2,
